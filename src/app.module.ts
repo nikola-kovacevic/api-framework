d783be24f69f0c697e2ed12439ea1dd895c96e2d
Nikola Kovacevic <nikolakovacevic.zg@gmail.com>
2020-01-20 16:24:43 +0100

    feat(db): implemented database connection and added configuration service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,50 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import * as Joi from '@hapi/joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        MONGO_URL: Joi.string().required(),
+        MONGO_DB_NAME: Joi.string().required(),
+        MONGO_USER: Joi.string().required(),
+        MONGO_PASSWORD: Joi.string().required(),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri:
+          'mongodb+srv://' +
+          config.get<string>('MONGO_USER') +
+          ':' +
+          config.get<string>('MONGO_PASSWORD') +
+          '@' +
+          config.get<string>('MONGO_URL') +
+          '/' +
+          config.get<string>('MONGO_DB_NAME'),
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+
+import * as helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const port = parseInt(configService.get('PORT')) || 3000;
+
+  app.use(helmet());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      validationError: {
+        target: false,
+        value: false,
+      },
+    }),
+  );
+
+  app.setGlobalPrefix('v1');
+
+  await app
+    .listen(port)
+    .then(data => console.log(`Working on port: ${port} \nApplication data: ${JSON.stringify(data)}`));
 }
 bootstrap();
